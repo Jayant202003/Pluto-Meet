0d55035afda0731a34a324be0f3fbd78c1f0de5d
Big Dawg Jayant <jayantshivankar4@gmail.com>
2026-08-02 09:42:23 +0530

    Point frontend to deployed backend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
+    "https://pluto-meet-backend.onrender.com" :
     "http://localhost:8000"
-
 
 export default server;
